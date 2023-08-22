1d7c84dfb6d4f3e0478a8f4309b1cc834c641234
Caroline <ambar.caroline@gmail.com>
2023-08-22 15:37:47 -0300

    Add categories functionality

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const button = document.querySelector('[data-add-button]')
 const list = document.querySelector('[data-list]')
+const categoriesList = document.querySelector('[data-categories]')
+const buttonAddCategory = document.querySelector('[data-add-categories]')
+const categoryTitle = document.querySelector('[data-category-title]')
 
 const categories = [
   {
+    id: 1,
     category: "Tarefas de Casa",
-    itemList: "",
-    itemCheck: ""
+    items: [
+      {
+        id: 1,
+        item: "Limpar a sala",
+        checked: true,
+      }
+    ]
   },
 ]
 
@@ -49,3 +58,61 @@ button.addEventListener('click', () => {
   inputField.focus();
 })
 
+const handleActiveCategory = (categoryElement, currentCategory) => {
+  categoriesList.childNodes.forEach((categoryNode) => {
+    if(categoryNode.classList) {
+      categoryNode.querySelector('.sideMenu__category').classList.remove('sideMenu__category--active')
+      categoryTitle.innerHTML = currentCategory.category
+    }
+  })
+  categoryElement.querySelector('.sideMenu__category').classList.add('sideMenu__category--active')
+}
+
+
+
+const createCategoryElement = (element) => {
+
+
+  const categoryElement = document.createElement('li')
+  const currentCategory = categories.find((category) => category.id === element.id)
+
+  const categoryInput = document.createElement('input')
+  categoryInput.classList.add('navInput')
+  categoryInput.addEventListener('keyup', (e) => {
+    currentCategory.category = e.target.value
+  })
+  categoryElement.appendChild(categoryInput)
+  categoryElement.addEventListener('click', () => handleActiveCategory(categoryElement, currentCategory))
+
+  
+
+  const categoryButton = document.createElement('button')
+  categoryButton.innerHTML = `
+    <span class="sideMenu__category__counter">1/4</span>
+  `
+
+  categoryButton.classList.add('sideMenu__category')
+  categoryButton.prepend(categoryInput)
+
+  categoryElement.appendChild(categoryButton)
+
+  return categoryElement
+}
+
+
+
+const handleAddCategory = () => {
+  const newId = categoriesList.childElementCount + 1
+  const newCategory = {
+    id: newId,
+    category: '',
+    items: []
+  }
+  categories.push(newCategory)
+  const categoryElement = createCategoryElement(newCategory)
+  categoriesList.appendChild(categoryElement)
+  console.log(newCategory)
+}
+
+
+buttonAddCategory.addEventListener('click', handleAddCategory)
